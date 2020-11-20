a91f1d9bf899bc00a9186e8b773138db603492eb
sneeu-leeu <sa070197@gmail.com>
2020-11-20 14:48:13 +0200

    Add bot.rb detup

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,1 +1,29 @@
-require 'telegram/bot'
+require 'telegram_bot'
+
+class Bot
+  attr_reader :bot, :get_updates
+
+  def initialize
+    token = '1402298027:AAHBWwLSVVGmeZKi8Kmog5HgYja7VmK6WlY'
+    @bot = TelegramBot.new(token: token)
+    @get_updates = update
+  end
+
+  private
+
+  def update
+    @bot.get_updates(fail_silently: true) do |message|
+      puts "@#{message.from.username}: #{message.text}"
+      command = message.get_command_for(@bot)
+
+      message.reply do |reply|
+        case command
+
+        when /start/i
+          instructions = ['Hi, Im EazyMacBot! type /help to see what I can do']
+          reply.text = "#{instructions.sample.capitalize},
+      #{message.from.first_name}!"
+
+    end
+  end
+end
